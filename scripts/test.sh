2a4a495f3a222311224836b99e05dac66ae5f149
xysong <xysong25@gmail.com>
2023-09-06 12:30:40 -0400

    Update: test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,7 @@
-# baseline 
+# baseline
 export save=./test/${RANDOM}/
 mkdir -p $save
-python ./src/test_detection.py --outf $save --dataset $1 --out_dataset $2 --pre_trained_net $3  --dataroot ./data   2>&1 | tee  $save/log.txt
+
+# FashionMNIST
+python ./src/test_detection.py --outf $save --dataset $1 --out_dataset $2 --pre_trained_net $3  --num_classes 8 --num_channels 1 --dataroot ./data   2>&1 | tee  $save/log.txt
+# cmd line: bash test.sh --dataset FashionMNIST --out_dataset FashionMNIST --pre_trained_net results/joint_confidence_loss/17239/model_epoch_100.pth
--- src/test_detection.py
+++ src/test_detection.py
@@ -1,7 +1,7 @@
 ###################################################################################################
 # Measure the detection performance: reference code is https://github.com/ShiyuLiang/odin-pytorch #
 ###################################################################################################
-# Writer: Kimin Lee 
+# Writer: Kimin Lee
 from __future__ import print_function
 import argparse
 import torch
@@ -14,25 +14,34 @@ import torchvision.utils as vutils
 import calculate_log as callog
 import models
 import math
-
-from torch.utils.serialization import load_lua
+from data_loader import *
 from torchvision import datasets, transforms
 from torch.nn.parameter import Parameter
 from torch.autograd import Variable
 from numpy.linalg import inv
 
 # Training settings
-parser = argparse.ArgumentParser(description='Test code - measure the detection peformance')
+parser = argparse.ArgumentParser(
+    description='Test code - measure the detection peformance')
 parser.add_argument('--batch-size', type=int, default=128, help='batch size')
-parser.add_argument('--no-cuda', action='store_true', default=False, help='disables CUDA')
-parser.add_argument('--seed', type=int, default=1,help='random seed')
-parser.add_argument('--dataset', required=True, help='target dataset: cifar10 | svhn')
+parser.add_argument('--no-cuda', action='store_true',
+                    default=False, help='disables CUDA')
+parser.add_argument('--seed', type=int, default=1, help='random seed')
+parser.add_argument('--dataset', required=True,
+                    help='target dataset: cifar10 | svhn')
 parser.add_argument('--dataroot', required=True, help='path to dataset')
-parser.add_argument('--imageSize', type=int, default=32, help='the height / width of the input image to network')
-parser.add_argument('--outf', default='/home/rack/KM/2017_Codes/overconfidence/test/log_entropy', help='folder to output images and model checkpoints')
-parser.add_argument('--out_dataset', required=True, help='out-of-dist dataset: cifar10 | svhn | imagenet | lsun')
-parser.add_argument('--num_classes', type=int, default=10, help='number of classes (default: 10)')
-parser.add_argument('--pre_trained_net', default='', help="path to pre trained_net")
+parser.add_argument('--imageSize', type=int, default=32,
+                    help='the height / width of the input image to network')
+parser.add_argument('--outf', default='/home/rack/KM/2017_Codes/overconfidence/test/log_entropy',
+                    help='folder to output images and model checkpoints')
+parser.add_argument('--out_dataset', required=True,
+                    help='out-of-dist dataset: cifar10 | svhn | imagenet | lsun')
+parser.add_argument('--num_classes', type=int, default=10,
+                    help='number of classes (default: 10)')
+parser.add_argument('--num_channels', type=int,
+                    default=3, help='# of channels')
+parser.add_argument('--pre_trained_net', default='',
+                    help="path to pre trained_net")
 
 args = parser.parse_args()
 print(args)
@@ -46,24 +55,54 @@ if args.cuda:
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 
 print('Load model')
-model = models.vgg13()
+# model = models.vgg13()
+model = models.densenet.DenseNet3(
+    depth=100, num_classes=args.num_classes, input_channel=args.num_channels)
 model.load_state_dict(torch.load(args.pre_trained_net))
 print(model)
 
-print('load target data: ',args.dataset)
-_, test_loader = data_loader.getTargetDataSet(args.dataset, args.batch_size, args.imageSize, args.dataroot)
+print('load target data: ', args.dataset)
+if args.dataset == 'CIFAR10-SVHN' or args.dataset == 'MNIST-FashionMNIST':
+    dset = DSET(args.dataset, False, args.batch_size,
+                args.batch_size, None, None)
+    train_loader, test_loader = dset.ind_train_loader, dset.ind_val_loader
+elif args.dataset == 'SVHN' or args.dataset == 'FashionMNIST':
+    dset = DSET(args.dataset, True, args.batch_size,
+                args.batch_size, [0, 1, 2, 3, 4, 5, 6, 7], [8, 9])
+    train_loader, test_loader = dset.ind_train_loader, dset.ind_val_loader
+elif args.dataset == 'MNIST':
+    dset = DSET(args.dataset, True, args.batch_size,
+                args.batch_size, [2, 3, 6, 8, 9], [1, 7])
+    train_loader, test_loader = dset.ind_train_loader, dset.ind_val_loader
+else:
+    assert False
 
-print('load non target data: ',args.out_dataset)
-nt_test_loader = data_loader.getNonTargetDataSet(args.out_dataset, args.batch_size, args.imageSize, args.dataroot)
+print('load non target data: ', args.out_dataset)
+
+if args.dataset == 'CIFAR10-SVHN' or args.dataset == 'MNIST-FashionMNIST':
+    dset = DSET(args.dataset, False, args.batch_size,
+                args.batch_size, None, None)
+    nt_test_loader = dset.ood_val_loader
+elif args.dataset == 'SVHN' or args.dataset == 'FashionMNIST':
+    dset = DSET(args.dataset, True, args.batch_size,
+                args.batch_size, [0, 1, 2, 3, 4, 5, 6, 7], [8, 9])
+    nt_test_loader = dset.ood_val_loader
+elif args.dataset == 'MNIST':
+    dset = DSET(args.dataset, True, args.batch_size,
+                args.batch_size, [2, 3, 6, 8, 9], [1, 7])
+    nt_test_loader = dset.ood_val_loader
+else:
+    assert False
 
 if args.cuda:
     model.cuda()
+
 
 def generate_target():
     model.eval()
     correct = 0
     total = 0
-    f1 = open('%s/confidence_Base_In.txt'%args.outf, 'w')
+    f1 = open('%s/confidence_Base_In.txt' % args.outf, 'w')
 
     for data, target in test_loader:
         total += data.size(0)
@@ -79,17 +118,19 @@ def generate_target():
         correct += equal_flag.sum()
         for i in range(data.size(0)):
             # confidence score: max_y p(y|x)
-            output = batch_output[i].view(1,-1)
+            output = batch_output[i].view(1, -1)
             soft_out = F.softmax(output)
             soft_out = torch.max(soft_out.data)
             f1.write("{}\n".format(soft_out))
 
-    print('\n Final Accuracy: {}/{} ({:.2f}%)\n'.format(correct, total, 100. * correct / total))
+    print('\n Final Accuracy: {}/{} ({:.2f}%)\n'.format(correct,
+          total, 100. * correct / total))
+
 
 def generate_non_target():
     model.eval()
     total = 0
-    f2 = open('%s/confidence_Base_Out.txt'%args.outf, 'w')
+    f2 = open('%s/confidence_Base_Out.txt' % args.outf, 'w')
 
     for data, target in nt_test_loader:
         total += data.size(0)
@@ -99,10 +140,11 @@ def generate_non_target():
         batch_output = model(data)
         for i in range(data.size(0)):
             # confidence score: max_y p(y|x)
-            output = batch_output[i].view(1,-1)
+            output = batch_output[i].view(1, -1)
             soft_out = F.softmax(output)
             soft_out = torch.max(soft_out.data)
             f2.write("{}\n".format(soft_out))
+
 
 print('generate log from in-distribution data')
 generate_target()
