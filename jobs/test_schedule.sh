5abc9a52d45c274bd3c0874ac5c3b2fbbb4ec05b
xysong <xysong25@gmail.com>
2023-09-07 09:54:05 -0400

    Update: test scripts

--- jobs/test_schedule.sh
+++ jobs/test_schedule.sh
@@ -1,12 +1,12 @@
 # Run test bash scripts
 # SVHN
-bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.001-28561/model_epoch_100.pth
-bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.01-30566/model_epoch_100.pth
-bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.1-15528/model_epoch_100.pth
-bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-1-32674/model_epoch_100.pth
+bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.001-28561/model_epoch_100.pth 8 3 SVHN-0.001-28561
+bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.01-30566/model_epoch_100.pth 8 3 SVHN-0.01-30566
+bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.1-15528/model_epoch_100.pth 8 3 SVHN-0.1-15528
+bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-1-32674/model_epoch_100.pth 8 3 SVHN-1-32674
 
 # CIFAR10-SVHN
-bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.001-9623/model_epoch_100.pth
-bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.01-14220/model_epoch_100.pth
-bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.1-6513/model_epoch_100.pth
-bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-1-10745/model_epoch_100.pth
+bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.001-9623/model_epoch_100.pth 10 3 CS-0.001-9623
+bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.01-14220/model_epoch_100.pth 10 3 CS-0.01-14220
+bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.1-6513/model_epoch_100.pth 10 3 CS-0.1-6513
+bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-1-10745/model_epoch_100.pth 10 3 CS-1-10745
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 # baseline
-export save=./test/${RANDOM}/
+export save=./test/$6/
 mkdir -p $save
 
 # FashionMNIST
-python ./src/test_detection.py --outf $save --dataset $1 --out_dataset $2 --pre_trained_net $3  --num_classes 8 --num_channels 1 --dataroot ./data   2>&1 | tee  $save/log.txt
+python ./src/test_detection.py --outf $save --dataset $1 --out_dataset $2 --pre_trained_net $3  --num_classes $4 --num_channels $5 --dataroot ./data   2>&1 | tee  $save/log.txt
 # cmd line: bash scripts/test.sh  FashionMNIST  FashionMNIST results/joint_confidence_loss/17239/model_epoch_100.pth
 
 # SVHN
