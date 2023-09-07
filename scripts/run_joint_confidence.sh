42b54be2f8c0d849da3205d3e5f05cf46f0bb62f
xysong <xysong25@gmail.com>
2023-09-06 22:23:03 -0400

    Update: 0.01

--- scripts/run_joint_confidence.sh
+++ scripts/run_joint_confidence.sh
@@ -7,4 +7,4 @@ mkdir -p $save
 # python ./src/run_joint_confidence.py --dataset SVHN --num_classes 8 --outf $save --dataroot ./data   2>&1 | tee  $save/log.txt
 
 # FashionMNIST
-python ./src/run_joint_confidence.py --dataset FashionMNIST --num_classes 8 --num_channels 1 --batch-size 256 --beta 0.1 --outf $save --dataroot ./data   2>&1 | tee  $save/log.txt
+python ./src/run_joint_confidence.py --dataset FashionMNIST --num_classes 8 --num_channels 1 --batch-size 256 --beta 0.01 --outf $save --dataroot ./data   2>&1 | tee  $save/log.txt
