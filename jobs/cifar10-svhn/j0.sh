462e2a54a9c9e39579af824e2d0292783084457e
xysong <xysong25@gmail.com>
2023-09-07 01:25:15 -0400

    Update: cifar10 svhn jobs

---
+++ jobs/cifar10-svhn/j0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --account=sunwbgt98
+#SBATCH --job-name=BASE-CS0
+#SBATCH --mail-user=xysong@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --nodes=1
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-gpu=14GB
+#SBATCH --time=24:00:00
+#SBATCH --output=/scratch/sunwbgt_root/sunwbgt98/xysong/jobs/BASE-CS0.log
+
+export save=./results/joint_confidence_loss/CS-0.001-${RANDOM}/
+mkdir -p $save
+# SVHN
+python ./src/run_joint_confidence.py --dataset CIFAR10-SVHN --num_classes 10 --batch-size 64 --beta 0.001 --outf $save --dataroot ./data   2>&1 | tee  $save/log.txt
