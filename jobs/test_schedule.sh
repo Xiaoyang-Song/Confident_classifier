4012632ce8dbc3b00e084ad7019b4d2b3a966221
xysong <xysong25@gmail.com>
2023-09-07 09:48:19 -0400

    Update: scheduler

---
+++ jobs/test_schedule.sh
@@ -0,0 +1,12 @@
+# Run test bash scripts
+# SVHN
+bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.001-28561/model_epoch_100.pth
+bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.01-30566/model_epoch_100.pth
+bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-0.1-15528/model_epoch_100.pth
+bash scripts/test.sh  SVHN SVHN results/joint_confidence_loss/SHVN-1-32674/model_epoch_100.pth
+
+# CIFAR10-SVHN
+bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.001-9623/model_epoch_100.pth
+bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.01-14220/model_epoch_100.pth
+bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-0.1-6513/model_epoch_100.pth
+bash scripts/test.sh  CIFAR10-SVHN CIFAR10-SVHN results/joint_confidence_loss/CS-1-10745/model_epoch_100.pth
