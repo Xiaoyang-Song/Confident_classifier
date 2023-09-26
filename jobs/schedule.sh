7f54ed956305e58f8c68afe4fcbbda450b0d4810
xysong <xysong25@gmail.com>
2023-09-26 00:36:34 -0400

    Update: validation

--- jobs/schedule.sh
+++ jobs/schedule.sh
@@ -21,7 +21,7 @@ export PYTHONPATH=$PYTHONPATH$:`pwd`
 sbatch jobs/cifar10-svhn/j1.sh
 sbatch jobs/cifar10-svhn/j2.sh
 sbatch jobs/cifar10-svhn/j3.sh
-# sbatch jobs/cifar10-svhn/j4.sh
+sbatch jobs/cifar10-svhn/j4.sh
 # sbatch jobs/cifar10-svhn/j5.sh
 sbatch jobs/cifar10-svhn/j6.sh
 sbatch jobs/cifar10-svhn/j7.sh
